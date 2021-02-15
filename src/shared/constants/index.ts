4135e786e20156db6c492089be2dfa9d7dc93218
victoria tapia <tapia.victoria5070@gmail.com>
2021-02-15 22:35:10 +0100

    update api key

--- src/shared/constants/index.ts
+++ src/shared/constants/index.ts
@@ -1,3 +1,5 @@
-export const API_KEY = process.env.REACT_APP_ALPH_VANTAGE_API_KEY;
+// Optimisation as a best practice
+// export const API_KEY = process.env.REACT_APP_ALPH_VANTAGE_API_KEY;
+export const API_KEY = 'DVLWHNCLMYOS1LEO';
 
 export const BASE_ENDPOINT = 'https://www.alphavantage.co/query';
